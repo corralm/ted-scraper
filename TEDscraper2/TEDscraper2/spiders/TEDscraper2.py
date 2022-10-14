14fa1a3935ef3086c5c126fc3d858ec3de96b65c
Miguel Corral Jr <corraljrmiguel@gmail.com>
2022-10-13 21:33:37 -0300

    docs: add comments and docstrings

--- TEDscraper2/TEDscraper2/spiders/TEDscraper2.py
+++ TEDscraper2/TEDscraper2/spiders/TEDscraper2.py
@@ -3,23 +3,33 @@ import scrapy
 
 
 class TEDscraper(scrapy.Spider):
+    """Recursively crawls ted.com/talks and extracts talk data."""
     name = 'TEDscraper'
     start_urls = [
         # 'https://www.ted.com/talks?page=1'
-        f'https://www.ted.com/talks?page={i}' for i in range(1, 160)
+        f'https://www.ted.com/talks?page={i}' for i in range(2, 3)
     ]
 
     def parse(self, response):
+        """Recursively follows links to all TED talks and extracts data from them."""
+
+        # follow all the links to each talk on the page calling the parse_talk callback for each of them
         talks_page_links = response.xpath("//a[@class=' ga-link']/@href")
         yield from response.follow_all(talks_page_links, self.parse_talk)
 
+        # looks for the link to the next page, builds a URL and yields a new request to the next page
         # pagination_links = response.css(".pagination__next")
         # yield from response.follow_all(pagination_links, self.parse)
 
     
-    def parse_talk(self, response): 
+    def parse_talk(self, response):
+        """Parses the response, extracting the scraped talk data as dicts."""
+
+        # find xpath to the JSON with talk data
         raw = response.xpath("//script[@type='application/json']/text()").get()
+        # cast the string 'raw' to a dict
         js = json.loads(raw)
+        # create dict with talk data of interest
         d = js['props']['pageProps']['videoData']
         yield {
             'talk_id': d['id'],
